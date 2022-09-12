c0f379817803ccab31e640328aaf70dd3b6c2a02
DESKTOP-3SFUC9M\tttri <tttriet1997@gmail.com>
2022-09-13 00:06:45 +0700

    done sign in with gg

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -1,14 +1,43 @@
+import * as Google from "expo-google-app-auth";
 import PropTypes from "prop-types";
-import React, { useContext } from "react";
-import { View, Text, Button } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
 import AuthContext from "../../auth";
+
+const YOUR_CLIENT_ID_HERE =
+  "396667099624-3ac76265s9efop5r2m7qggsccm7h2tr1.apps.googleusercontent.com";
 
 const SignIn = (props) => {
   const { signIn } = useContext(AuthContext);
+  const [state, setState] = useState();
+
+  const signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        clientId: YOUR_CLIENT_ID_HERE,
+        scopes: ["profile", "email"],
+      });
+      console.log({ result });
+      setState(result);
+      if (result.type === "success") {
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      console.log({ e });
+      return { error: true };
+    }
+  };
+
+  useEffect(() => {
+    // signInWithGoogleAsync();
+  }, []);
+
   return (
     <View style={{}}>
       <Text>Sign In</Text>
-      <Button onPress={() => signIn({ A: "1" })} title="Sign In"></Button>
+      <Button onPress={() => signInWithGoogleAsync()} title="Sign In"></Button>
     </View>
   );
 };
